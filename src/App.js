56b9d6cac309935ef0c899c581f040d419a32e1c
pgarbrecht <philgarbrecht@gmail.com>
2022-11-12 14:11:11 -0600

    moved product state from store to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Home from './components/Home'
 import Store from './components/Store'
@@ -12,7 +12,54 @@ import {
   Link,
 } from "react-router-dom";
 
-function App() {
+// Define baseURL
+let baseURL = process.env.REACT_APP_BACKEND_URL
+
+//Note it is intentional to use class component here instead of function component + useState hook, because of issues with converting data from back end
+class App extends Component {
+  constructor(props) {
+		super(props);
+    this.state = {
+      productList: [{
+          name: '',
+          description: '',
+          image: '',
+          price: '',
+          inStock: ''
+      }]
+    };
+  }
+
+  //Getting products from the database and updating state
+  getProducts = () => {
+    var headers = {}
+    fetch(baseURL, {
+        method : "GET",
+        mode: 'cors',
+        headers: headers
+    })
+
+    .then((res) => {
+        if (res.status === 200) {
+            return res.json();
+        } else {
+            return [];
+        }
+    })
+
+    .then((data) => {    
+        this.setState({
+            // grabbing data from db and updating state when components mount
+            productList: data.allProducts
+        })        
+    });
+  }
+
+  componentDidMount() {
+      this.getProducts();
+  }
+  
+  render() {
   return (
   <>
     <nav>
@@ -24,12 +71,15 @@ function App() {
     </nav>
     <Routes>
       <Route path="/" element={<Home/>} />
-      <Route path="/store" element={<Store/>} />
+      <Route path="/store" element={<Store
+        productList={this.state.productList}
+      />} />
       <Route path="/contact" element={<Contact/>} />
       <Route path="/admin/dashboard" element={<AdminDashboard/>} />
     </Routes>
   </>
   );
+  }
 }
 
 export default App;
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,66 +1,19 @@
-import React, { Component} from 'react';
+import React from 'react';
 import '../App.css';
 
-// Define baseURL
-let baseURL = process.env.REACT_APP_BACKEND_URL
+const Store = (props) => {
 
-//Note it is intentional to use class component here instead of function component + useState hook, because of issues with converting data from back end
-class Store extends Component {
-    constructor(props) {
-		super(props);
-        this.state = {
-            productList: [{
-                name: '',
-                description: '',
-                image: '',
-                price: '',
-                inStock: ''
-            }]
-        }
-    }
-
-    //Getting products from the database and updating state
-    getProducts = () => {
-        var headers = {}
-        fetch(baseURL, {
-            method : "GET",
-            mode: 'cors',
-            headers: headers
-        })
-
-        .then((res) => {
-            if (res.status === 200) {
-                return res.json();
-            } else {
-                return [];
-            }
-        })
-
-        .then((data) => {    
-            this.setState({
-                // grabbing data from db and updating state when components mount
-                productList: data.allProducts
-            })        
-        });
-    }
-
-    componentDidMount() {
-        this.getProducts();
-    }
-
-    render() {
         return (
             <>
                 <h1>Store</h1>
-                
-                {this.state.productList.map((product, index) => {
+                {props.productList.map((product, index) => {
                     return(
                         <p key={index} >{product.name}</p>
                     )
                 })}
             </>
         )
-    }
+    // }
 }
   
 export default Store;
