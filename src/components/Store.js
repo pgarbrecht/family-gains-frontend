59a42dfb7c4d0224c2c2c0414a909117020d4bbf
pgarbrecht <philgarbrecht@gmail.com>
2022-11-12 10:35:46 -0600

    dotenv setup and backend baseurl accessible

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 import '../App.css';
 
+// Define baseURL
+let baseURL = process.env.REACT_APP_BACKEND_URL
+
 function Store() {
 
-    let productList = [
-        {name: 'product 1 name'},
-        {name: 'product 2 name'}
-    ]
+    // Product array placeholder we will update using state hook
+    let productList = [{
+        name: '',
+        description: '',
+        image: '',
+        price: '',
+        inStock: ''
+    }]
     const [products, setProduct] = useState(productList)
+
+    //Getting products from the database and updating state
+    // getExercises = () => {
+    //     fetch(baseURL)
+
+    //     .then((res) => {
+    //         if (res.status === 200) {
+    //             return res.json();
+    //         } else {
+    //             return [];
+    //         }
+    //     })
+
+    //     .then((data) => {    
+    //         this.setState({
+    //             // grabbing data from db and updating state when components mount
+    //             customExercises: data.allExercises
+    //         })        
+    //     });
+    // }
 
     return (
     <>
@@ -20,6 +47,8 @@ function Store() {
         </>
         )
     })}
+
+    {console.log('here is baseURL:', baseURL)}
 
     </>
     )
