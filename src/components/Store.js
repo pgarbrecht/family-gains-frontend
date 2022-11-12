ec6c5cb2876e7d9adc679779560beb96c846a5c0
pgarbrecht <philgarbrecht@gmail.com>
2022-11-12 13:22:29 -0600

    store can render product info

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component} from 'react';
 import '../App.css';
 
 // Define baseURL
 let baseURL = process.env.REACT_APP_BACKEND_URL
 
-function Store() {
-
-    // Product array placeholder we will update using state hook
-    let productList = [{
-        name: '',
-        description: '',
-        image: '',
-        price: '',
-        inStock: ''
-    }]
-    const [products, setProduct] = useState(productList)
+//Note it is intentional to use class component here instead of function component + useState hook, because of issues with converting data from back end
+class Store extends Component {
+    constructor(props) {
+		super(props);
+        this.state = {
+            productList: [{
+                name: '',
+                description: '',
+                image: '',
+                price: '',
+                inStock: ''
+            }]
+        }
+    }
 
     //Getting products from the database and updating state
-    const getProducts = () => {
+    getProducts = () => {
         var headers = {}
         fetch(baseURL, {
             method : "GET",
@@ -34,33 +37,30 @@ function Store() {
         })
 
         .then((data) => {    
-            setProduct({
+            this.setState({
                 // grabbing data from db and updating state when components mount
                 productList: data.allProducts
             })        
         });
     }
 
-    useEffect(() => {
-        getProducts();
-    })
+    componentDidMount() {
+        this.getProducts();
+    }
 
-    return (
-    <>
-    <h1>Store</h1>
-
-    {console.log('here is products array: ', products)}
-
-    {products.map((product) => {
-        return(
-            <p key={product.name} >{product.name}</p>
+    render() {
+        return (
+            <>
+                <h1>Store</h1>
+                
+                {this.state.productList.map((product, index) => {
+                    return(
+                        <p key={index} >{product.name}</p>
+                    )
+                })}
+            </>
         )
-    })}
-
-    {console.log('here is baseURL:', baseURL)}
-
-    </>
-    )
+    }
 }
   
 export default Store;
