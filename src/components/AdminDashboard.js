dbbfd9efaccb9e2179e66ac587e7d6e44fc812fa
pgarbrecht <philgarbrecht@gmail.com>
2022-11-14 08:11:46 -0600

    clicking edit button loads modal with product data to edit

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Modal from "react-modal";
 import '../App.css';
 
+// backend url
 let baseURL = process.env.REACT_APP_BACKEND_URL
 
+// react modal styling
+const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "white",
+      width: 400,
+    },
+  };
+
 const AdminDashboard = (props) => {
+
+    // Using hook for our modal open state
+    const [modalOpen, setModalOpen] = useState(false);
 
     // Creating variable for new product, using hook to manage state
     let [productToAdd, setProductToAdd] = useState({
@@ -14,16 +33,34 @@ const AdminDashboard = (props) => {
         inStock: true
     })
 
-    // handleChange method
-    let handleChange = (e) => {
+    // Creating variable for product to edit, using hook to manage state
+    let [productToEdit, setProductToEdit] = useState({
+        id: '',
+        name: '',
+        description: '',
+        image: '',
+        price: '',
+        inStock: true
+    })
+
+    // handle change method for adding a product
+    let handleAddChange = (e) => {
         setProductToAdd({
             ...productToAdd,
             [e.target.id]: e.target.value
         })
     }
 
-    // handleSubmit method
-    let handleSubmit = (e) => {
+    // handle change method for editing a product
+    let handleEditChange = (e) => {
+        setProductToEdit({
+            ...productToEdit,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    // handle submit method for adding a product
+    let handleAddSubmit = (e) => {
         console.log(productToAdd)
         e.preventDefault()
         fetch(`${baseURL}/new`, {
@@ -57,9 +94,47 @@ const AdminDashboard = (props) => {
                 price: '',
                 inStock: '',
             }) 
-            window.location.href=`http://localhost:3000/admin/dashboard/`
+            window.location.href=`https://family-gains.herokuapp.com/admin/dashboard`
         })
         .catch(err => (console.log(err)))
+    }
+
+    // handle submit method for edited exercise
+    let handleEditProduct = (e) => {
+        e.preventDefault()
+        fetch(`${baseURL}/${productToEdit.id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name: productToEdit.name,
+                description: productToEdit.description,
+                image: productToEdit.image,
+                price: productToEdit.price,
+                inStock: productToEdit.inStock
+            }),
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+        })
+        .then(res => {
+            if (res.ok) {
+                return res.json()
+            } throw new Error(res)
+        })
+        .then(resJson => {
+            window.location.href=`https://family-gains.herokuapp.com/admin/dashboard`;
+        })
+        .catch(err => (console.log(err))) 
+    }
+
+    let passExerciseData = (productToEdit) => {
+        setProductToEdit({ 
+            id: productToEdit._id,
+            name: productToEdit.name,
+            description: productToEdit.description,
+            image: productToEdit.image,
+            price: productToEdit.price,
+            inStock: productToEdit.inStock,
+        })
     }
 
     let handleDeleteProduct = (id) => {
@@ -71,19 +146,23 @@ const AdminDashboard = (props) => {
         copyProductList.splice(findIndex, 1)
         props.setState({productList: copyProductList})
         console.log('got to bottom of handle delete');
-        }).then(window.location.href=`http://localhost:3000/admin/dashboard/`)
+        }).then(window.location.href=`https://family-gains.herokuapp.com/admin/dashboard/`)
     }
     
+    let sayHello = () => {
+        console.log('hello')
+    }
+
     return (
         <>
         <h1>Admin Dashboard</h1>
         <h2>Add a New Product</h2>
-        <form onSubmit={handleSubmit} >
+        <form onSubmit={handleAddSubmit} >
             <input
                 id='name'
                 type='text'
                 value={productToAdd.name}
-                onChange={handleChange}
+                onChange={handleAddChange}
                 placeholder='Product Name'
             >
             </input>
@@ -91,7 +170,7 @@ const AdminDashboard = (props) => {
                 id='description'
                 type='text'
                 value={productToAdd.description}
-                onChange={handleChange}
+                onChange={handleAddChange}
                 placeholder='Product Description'
                 >
             </input> 
@@ -99,7 +178,7 @@ const AdminDashboard = (props) => {
                 id='image'
                 type='text'
                 value={productToAdd.image}
-                onChange={handleChange}
+                onChange={handleAddChange}
                 placeholder='Product Image URL'
             >
             </input>
@@ -107,14 +186,14 @@ const AdminDashboard = (props) => {
                 id='price'
                 type='number'
                 value={productToAdd.notes}
-                onChange={handleChange}
+                onChange={handleAddChange}
                 placeholder='Product Price'
             >
             </input>
             {/* <select
                 id='inStock'
                 type='boolean'
-                onChange={handleChange}
+                onChange={handleAddChange}
                 placeholder='inStock'
             >
                 <option value="none" selected disabled hidden>In Stock?</option> */}
@@ -132,12 +211,72 @@ const AdminDashboard = (props) => {
         <h2>Manage Existing Products</h2>
         {props.productList.map((product, index) => {
             return(
-                <>
+                <div>
                 <p key={index} >{product.name}</p>
+                <span onClick={() => passExerciseData(product)}><button onClick={setModalOpen}>edit</button></span>
+                <Modal
+                    isOpen={modalOpen}
+                    onRequestClose={() => setModalOpen(false)}
+                    style={customStyles}
+                >
+                    <h2>Edit Product</h2>
+                    <button onClick={() => setModalOpen(false)}>Exit</button>
+                    <form onSubmit={handleAddSubmit} >
+                        <input
+                            id='name'
+                            type='text'
+                            value={productToEdit.name}
+                            onChange={handleEditChange}
+                            // placeholder={productToEdit.name}
+                        >
+                        </input>
+                        <input
+                            id='description'
+                            type='text'
+                            value={productToEdit.description}
+                            onChange={handleEditChange}
+                            // placeholder='Product Description'
+                            >
+                        </input> 
+                        <input
+                            id='image'
+                            type='text'
+                            value={productToEdit.image}
+                            onChange={handleEditChange}
+                            // placeholder='Product Image URL'
+                        >
+                        </input>
+                        <input
+                            id='price'
+                            type='number'
+                            value={productToEdit.price}
+                            onChange={handleEditChange}
+                            // placeholder='Product Price'
+                        >
+                        </input>
+                        {/* <select
+                            id='inStock'
+                            type='boolean'
+                            onChange={handleAddChange}
+                            placeholder='inStock'
+                        >
+                            <option value="none" selected disabled hidden>In Stock?</option> */}
+                            {/* <option value='true' type='boolean'>true</option>
+                            <option value='false' type='boolean'>false</option> */}
+                            {/* <option value='true' type='boolean'>true</option>
+                            <option value='false' type='boolean'>false</option> */}
+                        {/* </select> */}
+                        {/* <input type="checkbox" /> */}
+                        <input 
+                            type='submit'
+                            value='Submit Edited Product'
+                        />   
+                    </form>
+                </Modal>
                 <button onClick={()=> {handleDeleteProduct(product._id)}}>
                     delete
                 </button>
-                </>
+                </div>
             )
         })}
     </>
