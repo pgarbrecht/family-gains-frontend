874c8a4cdc9f412d393dd6bf8f02617d6285dcc5
pgarbrecht <philgarbrecht@gmail.com>
2022-11-22 20:46:55 -0600

    contact page styled

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../App.css';
+import FacebookIcon from '../images/facebook-icon.png';
+import InstagramIcon from '../images/instagram-icon.png';
 
 function Contact() {
     return (
-    <>
-    <h1>Contact</h1>
-    </>
+    <div className='h-screen flex flex-col justify-center items-center'>
+        <h1 className='text-3xl md:text-4xl text-center pb-8'>We'd Love to Hear From You!</h1>
+        <h2 className='text-xl md:text-1xl text-center pb-8'>Please email contact@familygains.com or reach us on social</h2>
+        <div className='flex'>
+            <a href='https://www.facebook.com/FamilyGains/' className='pr-8'><img src={FacebookIcon} id='social-icons'></img></a>
+            <a href='https://www.instagram.com/familygainsedu/'><img src={InstagramIcon} id='social-icons'></img></a>
+        </div>
+    </div>
     )
 }
   
