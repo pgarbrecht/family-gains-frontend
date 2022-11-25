98c15c6c06cd270b667e456b5b4a58149b46276b
pgarbrecht <philgarbrecht@gmail.com>
2022-11-25 10:25:59 -0600

    mobile home image size styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,13 @@ import '../App.css';
 function Home() {
     return (
     <div className='flex flex-col-reverse md:flex-row h-screen justify-center items-center'>
-        <div className='h-screen md:w-1/2 flex flex-col justify-center sm:justify-evenly items-start p-4 sm:p-16'>
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold font-Montserrat ">Resources to Raise Your Family</h1>
-            <div className='border-2 border-black w-full rounded-xl'></div>
-            <p>Raising a family is hard. We make it a little easier, with our educational activities and content to help your kids build academic skills, life skills, and positive values all while having fun!</p>
-            <button className=' p-4 rounded-full text-white bg-familygainsred hover:bg-familygainsdarkred'><Link to='/store'>View Store</Link></button>
+        <div className='h-screen md:w-1/2 flex flex-col justify-start sm:justify-evenly items-start p-4 sm:p-16'>
+            <h1 className="mb-4 sm:mb-0 text-3xl md:text-4xl lg:text-5xl font-bold font-Montserrat ">Resources to Raise Your Family</h1>
+            <div className='mb-4 sm:mb-0 border-2 border-black w-full rounded-xl'></div>
+            <p className='mb-4 sm:mb-0'>Raising a family is hard. We make it a little easier, with our educational activities and content to help your kids build academic skills, life skills, and positive values all while having fun!</p>
+            <button className='mb-4 sm:mb-0 p-4 rounded-full text-white bg-familygainsred hover:bg-familygainsdarkred'><Link to='/store'>View Store</Link></button>
         </div>
-        <img className='h-3/5 md:h-4/5 lg:h-5/5 p-16' src='https://i.ibb.co/jfQpRtg/Screenshot-2022-11-21-at-8-06-21-PM.png'></img>
+        <img className='h-2/5 md:h-4/5 lg:h-5/5 p-16' src='https://i.ibb.co/jfQpRtg/Screenshot-2022-11-21-at-8-06-21-PM.png'></img>
     </div>
     )
 }
