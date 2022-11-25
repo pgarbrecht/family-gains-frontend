a6990e997ab09e6f837c5110bca5adac781385b0
pgarbrecht <philgarbrecht@gmail.com>
2022-11-25 11:38:12 -0600

    update mobile store tile sizing

--- src/components/ProductPreview.js
+++ src/components/ProductPreview.js
@@ -5,7 +5,7 @@ import '../App.css';
 function ProductPreview(props) {
     return (
     <Link to ={`/store/${props.id}`}>
-    <div className='border-2 hover:border-4 hover:border-familygainsdarkred shadow-lg mt-12 ml-12 mr-12 sm:ml-4 sm:mr-8 rounded-lg flex flex-col items-center p-8 w-[350px] h-[350px] hover:cursor-pointer'>
+    <div className='border-2 hover:border-4 hover:border-familygainsdarkred shadow-lg mt-12 ml-12 mr-12 sm:ml-4 sm:mr-8 rounded-lg flex flex-col items-center p-8 sm:w-[350px] sm:h-[350px] hover:cursor-pointer'>
         <h2 key={props.index} className='text-3xl '>{props.name}</h2>
         <img src={props.image} className='w-[200px] h-[200px] mt-4 mb-4'></img>
     </div>
